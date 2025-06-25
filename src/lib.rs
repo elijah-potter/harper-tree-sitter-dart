1f13c4970efe9d49e2bdf57682d3d35ded3e65f3
Elijah Potter <me@elijahpotter.dev>
2025-06-25 09:11:35 -0600

    feat: initial commit

---
+++ src/lib.rs
@@ -0,0 +1,53 @@
+//! This crate provides Dart language support for the [tree-sitter][] parsing library.
+//!
+//! Typically, you will use the [LANGUAGE][] constant to add this language to a
+//! tree-sitter [Parser][], and then use the parser to parse some code:
+//!
+//! ```
+//! use tree_sitter::Parser;
+//!
+//! let code = r#"
+//! class Test {
+//!     int double(int x) {
+//!         return x * 2;
+//!     }
+//! }
+//! "#;
+//! let mut parser = Parser::new();
+//! let language = harper_tree_sitter_dart::LANGUAGE;
+//! parser
+//!     .set_language(&language.into())
+//!     .expect("Error loading Dart parser");
+//! let tree = parser.parse(code, None).unwrap();
+//! assert!(!tree.root_node().has_error());
+//! ```
+//!
+//! [Parser]: https://docs.rs/tree-sitter/*/tree_sitter/struct.Parser.html
+//! [tree-sitter]: https://tree-sitter.github.io/
+
+use tree_sitter_language::LanguageFn;
+
+extern "C" {
+    fn tree_sitter_dart() -> *const ();
+}
+
+/// The tree-sitter [`LanguageFn`][LanguageFn] for this grammar.
+///
+/// [LanguageFn]: https://docs.rs/tree-sitter-language/*/tree_sitter_language/struct.LanguageFn.html
+pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_dart) };
+
+/// The content of the [`node-types.json`][] file for this grammar.
+///
+/// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
+pub const NODE_TYPES: &str = include_str!("../grammar-src/node-types.json");
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_can_load_grammar() {
+        let mut parser = tree_sitter::Parser::new();
+        parser
+            .set_language(&super::LANGUAGE.into())
+            .expect("Error loading Dart parser");
+    }
+}
